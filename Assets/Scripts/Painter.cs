9cf157d92b45e198261a2d46ebc87e15fa141b5c
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-10-30 21:08:33 +0200

    Added painter visualizer.

--- Assets/Scripts/Painter.cs
+++ Assets/Scripts/Painter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Controller that serves as a tool for painting objects with Paintable layer.
@@ -6,9 +7,11 @@
 [RequireComponent(typeof(Camera))]
 internal sealed class Painter : MonoBehaviour
 {
+    public event Action<RaycastHit> PointerUpdatedPosition; 
     public Settings settings;
     private Camera camera;
     private Color32[] colors;
+    private int previousTriangleIndex = -1;
 
     private void Awake()
     {
@@ -17,34 +20,41 @@ internal sealed class Painter : MonoBehaviour
     
     private void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            Paint();
-        }
-    }
-
-    private void Paint()
-    {
         RaycastHit hit;
         Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 50.0f,  1 << 8))
+        if (Physics.Raycast(ray, out hit, 50.0f, 1 << 8))
         {
             Mesh mesh = hit.collider.GetComponent<MeshFilter>().mesh;
             if (colors == null)
             {
-                colors = new Color32[mesh.vertexCount];
-                for (int i = 0; i < colors.Length; i++)
-                {
-                    colors[i] = Color.white;
-                }
+                InitializeColors(mesh);
             }
 
-            int[] triangles = mesh.triangles;
-            colors[triangles[hit.triangleIndex * 3 + 0]] = settings.paintColor;
-            colors[triangles[hit.triangleIndex * 3 + 1]] = settings.paintColor;
-            colors[triangles[hit.triangleIndex * 3 + 2]] = settings.paintColor;
+            int triangleIndex = hit.triangleIndex;
+            if (triangleIndex != previousTriangleIndex)
+            {
+                previousTriangleIndex = triangleIndex;
+                PointerUpdatedPosition?.Invoke(hit);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                int[] triangles = mesh.triangles;
+                colors[triangles[hit.triangleIndex * 3 + 0]] = settings.paintColor;
+                colors[triangles[hit.triangleIndex * 3 + 1]] = settings.paintColor;
+                colors[triangles[hit.triangleIndex * 3 + 2]] = settings.paintColor;
             
-            mesh.colors32 = colors;
+                mesh.colors32 = colors;
+            }
+        }
+    }
+
+    private void InitializeColors(Mesh mesh)
+    {
+        colors = new Color32[mesh.vertexCount];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.white;
         }
     }
 }
---
+++ Assets/Scripts/PainterVisualizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class PainterVisualizer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject visualizerPrefab;
+    [SerializeField]
+    private Painter painter;
+    private Transform visualizer;
+    
+    private void Awake()
+    {
+        visualizer = Instantiate(visualizerPrefab).GetComponent<Transform>();
+        painter.PointerUpdatedPosition += OnPointerUpdatedPosition;
+    }
+
+    private void OnPointerUpdatedPosition(RaycastHit hit)
+    {
+        visualizer.transform.position = hit.point;
+        visualizer.transform.up = hit.normal;
+    }
+}
